85d98a9db348edc78253eefdaca79a3e48e3ee5d
DJ <114538201+dheerajnkn@users.noreply.github.com>
2026-08-16 17:04:07 -0500

    Wire live feeds and light liquid glass theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
+import './live.css'
+import './liquid.css'
+import './light.css'
 import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
